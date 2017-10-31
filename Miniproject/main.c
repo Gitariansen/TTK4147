e19f1fcbe3eea830867c083804e79f144f632752
gitariansen <elsa.ariansen@gmail.com>
2017-10-31 15:52:03 +0100

    fux the UDP-listener

--- Miniproject/main.c
+++ Miniproject/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "client/miniproject.h"
 
 #include <unistd.h>
@@ -7,33 +8,41 @@
 #include <pthread.h>
 #include <sched.h>
 #include <time.h>
+#include <semaphore.h>
 
 #define SERVER_PORT 	9999
 #define SERVER_IP 		"192.168.0.1"
 #define PERIOD 			10000
 #define REFERENCE		1
+#define RUNTIME_US		500000
 
 struct server_commands{
 	char* START;
 	char* GET;
+	char* GET_ACK;
 	char* SET;
 	char* STOP;
 };
 
 void *program_controller(void* commands);
 void *pi_controller();
-void *server_listener();
+void *server_listener(void* udp_connector);
 void *server_responder();
 void server_commands_init(struct server_commands *commands);
 
 float y;
 struct server_commands server_commands;
+sem_t PI_sem;
 
 /*
 #####################################
 ######## MAAAAAAIN LOOOOOP ##########
 #####################################
 */
+
+//TODO mutex udp??
+
+
 int main(){
 	printf("Starting tainyproject main..\n");
 	
@@ -44,23 +53,24 @@ int main(){
 	struct udp_conn UDP_connector;
 	udp_init_client(&UDP_connector, SERVER_PORT, SERVER_IP);
 	
+	// Init sem - move this somewhere nice
+	sem_init(&PI_sem, 0, 1);
+	
 	// Start simulation
 	udp_send(&UDP_connector, server_commands.START, strlen(server_commands.START) + 1);
 	
 	pthread_t program_controller_thread;
-	//pthread_t pi_controller_thread;
-	//pthread_t server_listener_thread;
+	pthread_t pi_controller_thread;
+	pthread_t server_listener_thread;
 	//pthread_t server_responder_thread;
 
 	pthread_create(&program_controller_thread, NULL, program_controller, &UDP_connector);
-	//pthread_create(&pi_controller_thread, NULL, pi_controller, NULL);
-	//pthread_create(&server_listener_thread, NULL, server_listener, NULL);
+	pthread_create(&pi_controller_thread, NULL, pi_controller, NULL);
+	pthread_create(&server_listener_thread, NULL, server_listener, &UDP_connector);
 	//pthread_create(&server_responder_thread, NULL, server_listener, NULL);
 	
 	pthread_join(program_controller_thread, NULL);
-	//pthread_join(pi_controller_thread, NULL);
-	//pthread_join(server_listener_thread, NULL);
-	//pthread_join(server_responder_thread, NULL);
+
 	
 	// Stop simulation & close udp connection
 	udp_send(&UDP_connector, server_commands.STOP, strlen(server_commands.STOP) + 1);
@@ -79,47 +89,50 @@ void *pi_controller(){
 	float integral = 0;
 	float error = 0;
 	float u = 0;
-
-
-	
-	error = REFERENCE - y;
-	integral = integral + (error * PERIOD);
-	u = k_p * error + k_i * integral;
-}
-
-void *server_listener(){
-	struct udp_conn *UDP_connector;
-
-	udp_init_client(&UDP_connector, 9999, "192.168.0.1");
-	
+	printf("PI-controller started..\n");
 	while(1){
-		udp_send(&UDP_connector, "random", 7);
-		sleep(1);
+		sem_wait(&PI_sem);
+		error = REFERENCE - y;
+		integral = integral + (error * PERIOD);
+		u = k_p * error + k_i * integral;
+		printf("value of u: %d \n", u);		
+		//TODO send u to udp
 	}
 	
-	udp_close(&UDP_connector);
 }
 
+
+void *server_listener(void* udp_connector){
+	int thing;
+	char y_str[32];
+	udp_conn udpConnector = *((udp_conn*)udp_connector);
+	char received_msg[32];
+	while(1){
+		if(thing = udp_receive(&udpConnector, received_msg, sizeof(received_msg))){
+			if(strncmp(received_msg, server_commands.GET_ACK, strlen(server_commands.GET_ACK)) == 0){
+				strncpy(y_str, (received_msg + 8), 8); //TODO generalise 8? split at ":"?
+				printf("Value of y:		%s\n", y_str);	
+				y = atof(y_str);
+				sem_post(&PI_sem);
+			}
+		}	
+	}
+
+}
 void *program_controller(void* udp_connector){
 
 	udp_conn udpConnector = *((udp_conn*)udp_connector);
 
 	struct timespec next;
-	struct timespec startTime;
-	struct timespec endTime;
+	int counter = 0;
 	clock_gettime(CLOCK_REALTIME, &next);
-	clock_gettime(CLOCK_REALTIME, &startTime);
 	
-	while(endTime.tv_nsec - startTime.tv_nsec < 500000000){
-		
+	while(counter < (RUNTIME_US/PERIOD)){
 		udp_send(&udpConnector, server_commands.GET, strlen(server_commands.GET) + 1);
-	
 		timespec_add_us(&next, PERIOD);
 		clock_nanosleep(&next);
-		clock_gettime(CLOCK_REALTIME, &endTime);
+		counter++;
 	}
-	
-
 }
 
 
@@ -129,8 +142,9 @@ void *server_responder(){
 }
 
 void server_commands_init(struct server_commands *commands){
-	commands->START = "START";
-	commands->GET = "GET";
-	commands->SET = "SET";
-	commands->STOP = "STOP";
+	commands->START 	= "START";
+	commands->GET 		= "GET";
+	commands->GET_ACK	= "GET_ACK";
+	commands->SET 		= "SET";
+	commands->STOP 		= "STOP";
 }
